4097e38f858e5d8a3955363d2e51998af18d7c94
Prathikbalaji <125907075+Prathikbalaji@users.noreply.github.com>
2024-08-26 16:37:54 +0530

    Add files via upload

---
+++ Daily Task/Day 6 -Task-26-08-24/ADO-Practices/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace ADO
+{
+    public class Program
+    {
+
+        static SqlConnection connection = null;
+        void getConn()
+        {
+            connection = new SqlConnection("data source = PTSQLTESTDB01 ; database = SPORTS_prathik ; integrated security = true");
+            connection.Open();
+        }
+
+        void PerformQuery()
+        {
+            SqlCommand sqlcd = new SqlCommand("select * from csharp", connection);
+
+            SqlDataReader sdr =  sqlcd.ExecuteReader();
+
+            while (sdr.Read()) {
+                Console.WriteLine(sdr[0].ToString() + " " + sdr[1].ToString());
+            }
+
+            connection.Close();
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.getConn();
+            p.PerformQuery();
+        }
+    }
+}
+
+
+
