97e8989b9a0dab5ee62293623dc296a1b8d4878d
Prathikbalaji <125907075+Prathikbalaji@users.noreply.github.com>
2024-08-27 15:42:56 +0530

    Add files via upload

---
+++ Daily Task/Day 7 -Task-27-08-24/ADO Windows Forms App/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace GridEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.products = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btbDelete = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.products)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // products
+            // 
+            this.products.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.products.Location = new System.Drawing.Point(239, 35);
+            this.products.Name = "products";
+            this.products.Size = new System.Drawing.Size(330, 150);
+            this.products.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(80, 50);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Product ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Price";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 92);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(80, 132);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(80, 221);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 7;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(187, 221);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 8;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(315, 221);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 9;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(412, 226);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(80, 275);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(187, 275);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btbDelete
+            // 
+            this.btbDelete.Location = new System.Drawing.Point(315, 275);
+            this.btbDelete.Name = "btbDelete";
+            this.btbDelete.Size = new System.Drawing.Size(75, 23);
+            this.btbDelete.TabIndex = 13;
+            this.btbDelete.Text = "Delete";
+            this.btbDelete.UseVisualStyleBackColor = true;
+            this.btbDelete.Click += new System.EventHandler(this.btbDelete_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = ":";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(575, 332);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btbDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.products);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.products)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView products;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btbDelete;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Daily Task/Day 7 -Task-27-08-24/ADO Windows Forms App/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridEx
+{
+    public partial class Form1 : Form
+    {
+
+        static SqlConnection connection = new SqlConnection("data source = PTSQLTESTDB01 ; database = SPORTS_prathik ; integrated security = true");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("insert into csharp values(@name , @price) ", connection);
+            sqlcd.Parameters.AddWithValue("@name", txtName.Text);
+            sqlcd.Parameters.AddWithValue("@price", txtPrice.Text);
+            sqlcd.ExecuteNonQuery();
+            connection.Close();
+            txtID.Text = "";
+            txtName.Text = "";
+            txtPrice.Text = "";
+
+            btnDisplay_Click(null, null);
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("select * from csharp", connection);
+
+            SqlDataReader sdr = sqlcd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+
+            products.DataSource = dt;
+
+            connection.Close();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("select count(*) from csharp", connection);
+
+            int sdr = (int)sqlcd.ExecuteScalar();
+
+            lblCount.Text = sdr.ToString();
+
+            connection.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("select * from csharp where id = @id", connection);
+            sqlcd.Parameters.AddWithValue("@id", txtID.Text);
+
+            SqlDataReader sdr = sqlcd.ExecuteReader();
+            while (sdr.Read())
+            {
+                txtName.Text = sdr[1].ToString();
+                txtPrice.Text = sdr[2].ToString();
+            }
+            connection.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("update csharp set Price = @price where id = @id", connection);
+            sqlcd.Parameters.AddWithValue("@id", txtID.Text);
+            sqlcd.Parameters.AddWithValue("@price", txtPrice.Text);
+
+            sqlcd.ExecuteNonQuery();
+            connection.Close();
+            btnDisplay_Click(null, null);
+        }
+
+        private void btbDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand sqlcd = new SqlCommand("delete from csharp where id = @id", connection);
+            sqlcd.Parameters.AddWithValue("@id", txtID.Text);
+            sqlcd.ExecuteNonQuery();
+            connection.Close();
+            btnDisplay_Click(null, null);
+        }
+
+    }
+}
