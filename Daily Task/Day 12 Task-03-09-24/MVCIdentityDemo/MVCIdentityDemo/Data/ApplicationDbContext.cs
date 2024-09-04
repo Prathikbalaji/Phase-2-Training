37a800ba5f6b3ccef098cf9a2d6ef44398ad87d7
Prathikbalaji <125907075+Prathikbalaji@users.noreply.github.com>
2024-09-04 11:00:18 +0530

    Add files via upload

---
+++ Daily Task/Day 12 Task-03-09-24/MVCIdentityDemo/MVCIdentityDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MVCIdentityDemo.Models;
+
+namespace MVCIdentityDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Daily Task/Day 12 Task-03-09-24/MVCIdentityDemo/MVCIdentityDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCIdentityDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpDesig { get; set; }
+    }
+}
